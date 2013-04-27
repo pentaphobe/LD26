46721d2bc5b483122dd27f303b60fded5d59efd0
pentaphobe <pentaphobe@gmail.com>
2013-04-27 23:03:06 +1000

    basic actor selection

--- src/scenes/PlayScene.hx
+++ src/scenes/PlayScene.hx
@@ -1,5 +1,6 @@
 
 package scenes;
+import nme.geom.Point;
 import com.haxepunk.Scene;
 import com.haxepunk.HXP;
 import com.haxepunk.utils.Input;
@@ -7,19 +8,27 @@ import com.haxepunk.utils.Key;
 import com.haxepunk.Entity;
 import com.haxepunk.graphics.Image;
 import com.haxepunk.tweens.motion.LinearMotion;
+import com.haxepunk.utils.Draw;
 
+import states.StateMachine;
+import states.PrototypeState;
+import states.UIState;
 import ui.Menu;
 import ui.UIEntity;
+import entities.Actor;
 import utils.ActorFactory;
 import com.haxepunk.Tween;
 
 
 class PlayScene extends Scene {
 	//***** TEMPORARY *******
-	var testEntity:Entity;
+	var testEntity:Actor;
+	var startDragPoint:Point;
+	var selectedEntities:Array<Entity>;
 	//***** /TEMPORARY ******
 
 	var menu:Menu;
+	var uiStates:StateMachine<UIState>;
 	public static var instance(get_instance, set_instance):PlayScene;
 	public static var TILE_SIZE:Int = 32;
 	public static var HTILE_SIZE:Int = cast (TILE_SIZE/2);
@@ -36,7 +45,37 @@ class PlayScene extends Scene {
 
 		loadLevelSet();
 		loadActorTemplates();
+
+		selectedEntities = new Array<Entity>();
+
+		uiStates = new StateMachine<UIState>("ui");
+		var testState:UIState = new UIState("test");
+		testState.setOverride(CustomUpdate, function (owner:PrototypeState) {
+			if (Input.mousePressed) {
+				startDragPoint = new Point(mouseX, mouseY);
+				if (!Input.check(Key.SHIFT)) {
+					selectedEntities = new Array<Entity>();
+				}
+			} else if (Input.mouseReleased) {
+				selectEntities(startDragPoint.x, startDragPoint.y, mouseX - startDragPoint.x, mouseY - startDragPoint.y);
+				startDragPoint = null;
+			}
+		});
+		testState.setOverride(CustomRender, function (owner:PrototypeState) {
+			if (startDragPoint != null && Input.mouseDown) {
+				var w:Int = cast(mouseX - startDragPoint.x);
+				var h:Int = cast(mouseY - startDragPoint.y);
+				Draw.rect(cast startDragPoint.x, cast startDragPoint.y, w, h, 0x00ff00, 0.1);
+			}
+		});
+		uiStates.addStateAndEnter(testState);
 	}	
+
+	public function selectEntities(x:Float, y:Float, w:Float, h:Float) {
+		
+		collideRectInto("computer", x, y, w, h, selectedEntities);
+
+	}
 
 	public function loadLevelSet() {
 		var levelsFile:Dynamic = Utils.loadJson("levels");
@@ -51,13 +90,16 @@ class PlayScene extends Scene {
 
 	public override function begin() {
 		super.begin();
+		uiStates.enter();
 		HXP.log("entering game");
 
 		setLevel(startLevelName);
 
-		var debug = Image.createRect(TILE_SIZE, TILE_SIZE, 0xff00ff);
-		testEntity = new Entity(HXP.screen.width / 2, HXP.screen.height / 2, debug);
+		testEntity = ActorFactory.create("basic", "computer", HXP.screen.width / 2, HXP.screen.height / 2);
 		add(testEntity);
+		testEntity = ActorFactory.create("scout", "computer", HXP.screen.width / 3, HXP.screen.height / 2);
+		add(testEntity);
+
 		// createMap();
 
 		// Keep this for last
@@ -65,6 +107,9 @@ class PlayScene extends Scene {
 	}
 
 	public function doAiMove(event:Dynamic) {
+		if (menu.isActive) {
+			return;
+		}
 		HXP.log("Ai update");
 		var newX:Float = testEntity.x + (Math.random()-0.5)*20;
 		var newY:Float = testEntity.y + (Math.random()-0.5)*20;
@@ -83,12 +128,18 @@ class PlayScene extends Scene {
 
 	public override function update() {
 		super.update();
+		uiStates.update();
 		// a little special case code for in-game menu since it acts differently
 		updateMenu();
 	}
 
 	public override function render() {
 		super.render();
+		uiStates.render();
+		for ( entity in selectedEntities) {
+			// Draw.hitbox(entity, true, 0x00ff00, 0.5);
+			Draw.circlePlus(cast entity.x, cast entity.y, TILE_SIZE+2, 0x00FF00, 0.25, false);
+		}
 		menu.render();
 	}
 
