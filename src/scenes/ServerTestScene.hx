e6aab165c7f5355d32cdba7d59895b215711cd6b
pentaphobe <pentaphobe@gmail.com>
2013-05-04 02:04:41 +1000

    experimenting, adding a simple dialog object

--- src/scenes/ServerTestScene.hx
+++ src/scenes/ServerTestScene.hx
@@ -7,10 +7,14 @@ import com.haxepunk.utils.Key;
 import com.haxepunk.Tween;
 import com.haxepunk.tweens.TweenEvent;
 import com.haxepunk.utils.Draw;
+import com.haxepunk.RenderMode;
 
 import server.Server;
 import server.ServerEventHandler;
 import server.ServerEvent;
+import ui.UIDialog;
+import ui.UIEntity;
+import ui.TextButton;
 
 /** This class just intercepts events and dumps them out
  */
@@ -34,22 +38,37 @@ class TestHandler extends BasicServerEventHandler {
 
 class ServerTestScene extends Scene {
 	var server:Server;
+	var dialog:UIDialog;
 	public function new() {
 		super();
 	}
 
 	public override function begin() {
-		server = new Server();
+		// server = new Server();
 
-		server.addHandler(new TestHandler());
+		// server.addHandler(new TestHandler());
 
 		// players aren't actors. stop confusing yourself :)
-		server.createPlayer("percival");
-		server.createPlayer("noggin");
+		// server.createPlayer("percival");
+		// server.createPlayer("noggin");
 
 		// no need to update at full speed, we just want to test
 		// server interaction
 		HXP.alarm(0.5, serverTick, TweenType.Looping);
+
+		dialog = new UIDialog(50, 50, 400, 50);
+		add(dialog);
+
+		dialog.add(new TextButton("hello there", "hello_button", 10, 10, function (s:String, ent:UIEntity):Void {
+			HXP.log(s);
+			if (s == "onClick") {
+				HXP.log("HELLO");
+			}
+		}));
+
+		if (HXP.renderMode.has(RenderMode.HARDWARE)) {
+			HXP.log("Hardware available");
+		}
 	}
 
 	public override function update() {
@@ -57,18 +76,23 @@ class ServerTestScene extends Scene {
 			HXP.scene = new MenuScene();
 		}
 		if (Input.pressed(Key.SPACE)) {
-			server.sendLocalOrder("hedge-trimmer");
-			server.sendLocalOrder("well-formed", 23, 23);
+			// server.sendLocalOrder("hedge-trimmer");
+			// server.sendLocalOrder("well-formed", 23, 23);
+		}
+		if (Input.mouseDown) {
+			var dx:Float = Input.mouseX - dialog.x;
+			var dy:Float = Input.mouseY - dialog.y;
+			dialog.setSize(dx, dy);
 		}
 	}
 
 	public override function render() {
-		Draw.text("press space to fire an event", HXP.screen.width/2, HXP.screen.height/2);
-
+		// Draw.text("press space to fire an event", HXP.screen.width/2, HXP.screen.height/2);
+		super.render();
 	}
 
 	public function serverTick(event:TweenEvent) {
-		server.update();
+		// server.update();
 	}
 
 	public override function end() {
--- src/ui/UIDialog.hx
+++ src/ui/UIDialog.hx
@@ -1,9 +1,33 @@
 
 package ui;
 import entities.EntityGroup;
+import com.haxepunk.HXP;
+import flash.geom.Rectangle;
+// import ext.com.kpulv.NineSlice;
+// import ext.com.haxepunk.NineSlice;
+import ui.NineSlice;
+// import com.haxepunk.graphics.atlas.Atlas;
+// import com.haxepunk.graphics.atlas.AtlasRegion;
 
 class UIDialog extends EntityGroup<UIEntity> {
 
-	
+	public function new(?x:Float=0, ?y:Float = 0, ?w:Float=0, ?h:Float=0) {
+		super(x, y);
+		
+		graphic = new NineSlice(cast w, cast h, new Rectangle(0, 0, 12, 12), "gfx/testslice.png");
+		// setHitbox(cast w, cast h, cast x, cast y);
+	}
+
+	public function setSize(width:Float, height:Float) {
+		cast(graphic, NineSlice).setSize(width, height);
+		setHitbox(cast width, cast height, 0, 0);
+	}
+
+	public override function add(e:UIEntity):UIEntity {
+		e.x += x;
+		e.y += y;
+		super.add(e);
+		return e;
+	}
 
 }
